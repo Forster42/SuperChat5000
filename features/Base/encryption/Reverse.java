f22687a18c1cc584fe12a541e1d87b4d3dc0835d
Forster42 <forsterolga@gmail.com>
2015-05-13 15:00:51 +0200

    Tacocat is reversed :-)

--- features/Base/encryption/Reverse.java
+++ features/Base/encryption/Reverse.java
@@ -11,18 +11,14 @@ public class Reverse implements Encryptor
     public String encrypt(String message)
     {
         //checking for a collon, since this indicates a username exists, therfore it is not DISCONNECT_REQ / DISCONNECT_ACKF
-        if (message.contains(":")) {
             //split at fors colon
-            String[] messageSplice = message.split(":", 2);
-            char[] choppedMessageContent = messageSplice[1].toCharArray();
+            char[] choppedMessageContent = message.toCharArray();
             char[] invertedMessageContent = new char[choppedMessageContent.length];
             for (int i = 0; i < choppedMessageContent.length; i++) {
                 invertedMessageContent[invertedMessageContent.length - i - 1] = choppedMessageContent[i];
             }
-            return messageSplice[0] + ":" + new String(invertedMessageContent);
-        }
-        return message;
-    }
+            return new String(invertedMessageContent);
+     }
 
     /**
      * Using a symmetric encryption. Decryption method may internally use
